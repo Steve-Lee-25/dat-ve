edfb75823554d63951e3567973b708d502e9f5f7
VanTrung11212 <emtenhuy.bos@gmail.com>
2022-06-09 12:06:42 +0700

    dat ve project commit

--- src/API/request.js
+++ src/API/request.js
@@ -11,7 +11,7 @@ const request = ({ url, body, params, method }) => {
   const token_2 = localStorage.getItem("user");
   if (token) {
     variables.headers = {
-      TokenCybersoft: token,
+      TokenCybersoft: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJEYXRhIFNjaWVuY2UgMDIiLCJIZXRIYW5TdHJpbmciOiIwNS8wOS8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NjIzMzYwMDAwMDAiLCJuYmYiOjE2NDU2MzU2MDAsImV4cCI6MTY2MjQ4MzYwMH0.8S-EqdxqvVcyITZdLnF7yPG1DEA7beD2AGxdbZzftkE',
       Authorization: "Bearer " + token_2,
     };
   }
